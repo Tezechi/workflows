f22df73d01982b35d682a6e817f08e4f6b1d381a
Tochukwu Ezechi <tezechi@toadeznet.com>
2018-03-11 13:06:24 +0100

    Concatenated JavaScript Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
     var coffeeSources = ['components/coffee/tagline.coffee']
+    var jsSources = [
+        'components/scripts/rclick.js',
+        'components/scripts/pixgrid.js',
+        'components/scripts/tagline.js',
+        'components/scripts/template.js'
+    ];
 
 //gulp.task('log', function(){
 //gutil.log('Workflows are awesome');
@@ -14,3 +21,9 @@ gulp.task('coffee', function(){
          .on('error', gutil.log)) // bare tells coffee to process it to js file without wrapper
         .pipe(gulp.dest('components/scripts'))  
         });
+
+gulp.task('js', function(){
+   gulp.src(jsSources)
+   .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('builds/development/js'))
+});
